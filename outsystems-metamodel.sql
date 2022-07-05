027f36d7b0b92b6e90b5d9be03cc9f308d54e3ec
nfs951168 <86299467+nfs951168@users.noreply.github.com>
2022-07-05 15:42:31 +0100

    Update outsystems-metamodel.sql
    Fix on integration references query

--- outsystems-metamodel.sql
+++ outsystems-metamodel.sql
@@ -110,23 +110,22 @@ AND		R.IS_ACTIVE = 1
 AND		U.IS_ACTIVE = 1
 
 
- 
-------------------------------------------------------------------------------------------------------------------------------------------------
+ ------------------------------------------------------------------------------------------------------------------------------------------------
 --Integrations: Get consumed web services information
 -------------------------------------------------------------------------------------------------------------------------------------------------
  
 DECLARE @webServiceName AS VARCHAR(100);
 
-SET @webServiceName = 'client_api';
+SET @webServiceName = 'CandidateTest';
 
 WITH cte_integrations AS (
 	SELECT	'REST' AS IntegrationType, name as IntegrationName, ESPACE_ID, [URL], EFFECTIVE_URL, IS_ACTIVE, SS_KEY
 	FROM	OSSYS_REST_WEB_REFERENCE
-	WHERE	effective_url LIKE '%' + @webServiceName + '%' OR	(ISNULL(effective_url, '') = '' AND EFFECTIVE_URL LIKE '%' + @webServiceName + '%')
+	WHERE	effective_url LIKE '%' + @webServiceName + '%' OR	(ISNULL(effective_url, '') = '' AND [URL] LIKE '%' + @webServiceName + '%')
 	UNION ALL
 	SELECT	'SOAP' AS IntegrationType, name as IntegrationName, ESPACE_ID, [URL], EFFECTIVE_URL, IS_ACTIVE, SS_KEY
 	FROM	ossys_web_reference
-	WHERE	effective_url LIKE '%' + @webServiceName + '%' OR	(ISNULL(effective_url, '') = '' AND EFFECTIVE_URL LIKE '%' + @webServiceName + '%')
+	WHERE	effective_url LIKE '%' + @webServiceName + '%' OR	(ISNULL(effective_url, '') = '' AND [URL] LIKE '%' + @webServiceName + '%')
 )
 
 
@@ -145,6 +144,7 @@ FROM	cte_integrations wr inner join ossys_espace es on (es.Id = wr.espace_id)
 WHERE	1 = 1
 --active webreferences
 and		wr.IS_ACTIVE = 1; 
+
 
  ------------------------------------------------------------------------------------------------------------------------------------------------
 --References: Get references of given module and object
