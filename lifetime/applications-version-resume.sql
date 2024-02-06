07603b843b14abc890f6d44d196b669c3b8c8454
Nuno Fernandes <86299467+nfs951168@users.noreply.github.com>
2024-02-06 16:05:45 +0000

    creation

---
+++ lifetime/applications-version-resume.sql
@@ -0,0 +1,13 @@
+select top 100 *
+from	OSLTM_APPLICATIONVERSION
+where	applicationId = 475
+
+
+
+---------------------------------------------------------------------------------------------------
+--Get all versions from development
+---------------------------------------------------------------------------------------------------
+select	top 100 * 
+from	OSLTM_ENVIRONMENTAPPVERSION eav
+
+
